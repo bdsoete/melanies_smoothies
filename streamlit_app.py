bd59be027513ec5db8a076ebfa11c4be9befa52e
bdsoete <bart.desoete@datasense.be>
2026-03-19 22:55:08 +0100

    Update streamlit_app.py
    Add API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert to pandas
 fo_df = my_dataframe.to_pandas()
-st.dataframe(fo_df)
-st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients: ',
                                   my_dataframe,
@@ -47,8 +45,11 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on = fo_df.loc[fo_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ' + fruit_chosen + ' is ' + search_on + '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
