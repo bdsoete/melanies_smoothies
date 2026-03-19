859b3d6e01f236b4ebe6e2f639130c5b9c6621bc
bdsoete <bart.desoete@datasense.be>
2026-03-19 22:17:09 +0100

    Update streamlit_app.py
    Add API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
@@ -54,7 +56,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
-smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
